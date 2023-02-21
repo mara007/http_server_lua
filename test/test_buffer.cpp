fc3d0ca021e10ccb93e25e6ce05601699b28f59a
Marek <mara007@github.io>
2023-02-21 14:34:16 +0100

    test_buffer - fixed tri-state of buffer_t.add_data()

--- test/test_buffer.cpp
+++ test/test_buffer.cpp
@@ -5,6 +5,8 @@
 
 #include "gtest/gtest.h"
 
+using my_http_buffer_t = http_buffer_t<1024>;
+
 TEST(http_buffer_t, msg_assemble_one) {
 
     std::shared_ptr<http_req_t> msg;
@@ -14,7 +16,7 @@ TEST(http_buffer_t, msg_assemble_one) {
         ++cb_called_times;
         std::cerr << "new msg cb fired.." << std::endl;
     };
-    http_buffer_t<1024> http_buf(cb);
+    my_http_buffer_t http_buf(cb);
 
     std::string fragment1 = "GET /path/to/resource HTTP/1.1\r\n\
 Host: example.com\r\n\
@@ -27,15 +29,15 @@ Pragma: no-cache\r\n";
     std::string fragment3 = "Cache-Control: no-cache\r\n\r\n";
 
     std::cout << "..first fragment\n";
-    ASSERT_TRUE(http_buf.new_data(fragment1.c_str(), fragment1.size()));
+    ASSERT_EQ(http_buf.new_data(fragment1.c_str(), fragment1.size()), my_http_buffer_t::new_data_state_e::NEED_MORE_DATA);
     ASSERT_EQ(cb_called_times, 0);
 
     std::cout << "..second fragment\n";
-    ASSERT_TRUE(http_buf.new_data(fragment2.c_str(), fragment2.size()));
+    ASSERT_EQ(http_buf.new_data(fragment2.c_str(), fragment2.size()), my_http_buffer_t::new_data_state_e::NEED_MORE_DATA);
     ASSERT_EQ(cb_called_times, 0);
 
     std::cout << "..final fragment\n";
-    ASSERT_TRUE(http_buf.new_data(fragment3.c_str(), fragment3.size()));
+    ASSERT_EQ(http_buf.new_data(fragment3.c_str(), fragment3.size()), my_http_buffer_t::new_data_state_e::MSG_OK);
     ASSERT_EQ(cb_called_times, 1);
     ASSERT_EQ(msg->method, "get");
     ASSERT_EQ(msg->headers.size(), 5);
@@ -66,15 +68,15 @@ GET /this/is/another/msg HTTP/1.1\r\n\
 Host: sample.com\r\n\r\n";
 
     std::cout << "..first fragment\n";
-    ASSERT_TRUE(http_buf.new_data(fragment1.c_str(), fragment1.size()));
+    ASSERT_EQ(http_buf.new_data(fragment1.c_str(), fragment1.size()), my_http_buffer_t::new_data_state_e::NEED_MORE_DATA);
     ASSERT_EQ(cb_called_times, 0);
 
     std::cout << "..second fragment\n";
-    ASSERT_TRUE(http_buf.new_data(fragment2.c_str(), fragment2.size()));
+    ASSERT_EQ(http_buf.new_data(fragment2.c_str(), fragment2.size()), my_http_buffer_t::new_data_state_e::NEED_MORE_DATA);
     ASSERT_EQ(cb_called_times, 0);
 
     std::cout << "..final fragment\n";
-    ASSERT_TRUE(http_buf.new_data(fragment3.c_str(), fragment3.size()));
+    ASSERT_EQ(http_buf.new_data(fragment3.c_str(), fragment3.size()), my_http_buffer_t::new_data_state_e::MSG_OK);
     ASSERT_EQ(cb_called_times, 2);
     ASSERT_EQ(msg->method, "get");
     ASSERT_EQ(msg->headers.size(), 1);
--- test/test_lua.cpp
+++ test/test_lua.cpp
@@ -1,4 +1,5 @@
 #include "lua/lua_manager.h"
+#include "http/http_message.h"
 
 #include <iostream>
 
@@ -13,4 +14,15 @@
 TEST(lua_manager_t, load_script) {
     auto lua_man = lua_manager_t();
     ASSERT_TRUE(lua_man.init(TEST_DIR_NAME"/script.lua", 1));
+}
+
+TEST(lua_manager_t, invoke_script) {
+    auto lua_man = lua_manager_t();
+    ASSERT_TRUE(lua_man.init(TEST_DIR_NAME"/script.lua", 1));
+
+    http_req_t http_req;
+    http_resp_t http_resp;
+
+    lua_man.invoke_script(http_req, http_resp);
+
 }
